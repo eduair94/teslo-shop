a043114f3b391022f49e8119dd1d2773a54d0e65
EduAir94 <49965850+EduAir94@users.noreply.github.com>
2023-12-25 22:42:15 -0300

    fix: top loader

--- components/provider/Providers.tsx
+++ components/provider/Providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import { SessionProvider } from 'next-auth/react';
+import NextTopLoader from 'nextjs-toploader';
 import { FC, ReactNode } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -13,6 +14,7 @@ interface Props {
 export const Providers: FC<Props> = ({ children }) => {
   return (
     <>
+      <NextTopLoader />
       <PayPalScriptProvider
         options={{
           clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID ?? '',
